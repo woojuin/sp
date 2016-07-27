cb60de3bf303c8c39c9f787a52f93bc395050c43
woojuin <woojuin@neowiz.com>
2016-07-27 14:39:35 +0900

    a

--- src/main/java/com/example/sp.java
+++ src/main/java/com/example/sp.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class sp {
-  @RequestMapping("/")
+  @RequestMapping("/test")
   public String home() {
     return ("SP OPENSHIFT");
   }
